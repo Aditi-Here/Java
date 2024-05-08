b1403a9a543f2d34627347d4ae6353dc717672e0
Aditi-Here <66709120+Aditi-Here@users.noreply.github.com>
2024-05-08 10:39:29 -0400

    Update PrimeNumbers.java
    added code

--- LearnYardClassPractice/src/Jan10/PrimeNumbers.java
+++ LearnYardClassPractice/src/Jan10/PrimeNumbers.java
@@ -2,13 +2,19 @@ package Jan10;
 import java.util.Scanner;
 
 public class PrimeNumbers {
+	static boolean isPrime(int n) {
+		if(n<=1) return false;
+		for(int i = 2; i<n/2; i++) {
+			if(n%i==0) return false;
+		}
+		return true;
+	}
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
 		int input_number = sc.nextInt();
-		for(int i=2;i<input_number;i++) {
-			
-		}
+		boolean ans = test.isPrime(input_Number);
+		System.out.println(ans);
 	}
 
 }
